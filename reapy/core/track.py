acb4ba40a58c845dcc224b4e6b53073919019589
RomeoDespres <romeo.despres@ens-rennes.fr>
2019-02-07 17:16:52 +0100

    Add Track.color getter

--- reapy/core/track.py
+++ reapy/core/track.py
@@ -1,3 +1,4 @@
+import reapy
 from reapy import reascript_api as RPR
 
 
@@ -7,6 +8,24 @@ class Track:
         if isinstance(id, int):
             id = RPR.GetTrack(project.id, id)
         self.id = id
+        
+    @property
+    def color(self):
+        """
+        Return track color in RGB format.
+        
+        Returns
+        -------
+        r : int
+            Red value of track color.
+        g : int
+            Green value of track color.
+        b : int
+            Blue value of track color.
+        """
+        native_color = RPR.GetTrackColor(self.id)
+        r, g, b = reapy.rgb_from_native(native_color)
+        return r, g, b
         
     def add_item(self):
         """
