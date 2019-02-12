cd933432363be4619b8266aff160eaba82fca9c2
RomeoDespres <romeo.despres@ens-rennes.fr>
2019-02-12 21:40:25 +0100

    Update compare_apis.py

--- compare_apis.py
+++ compare_apis.py
@@ -1,6 +1,7 @@
 import os, pandas as pd
 
 api = pd.read_csv(os.path.join(os.path.dirname(__file__), "docs", "api.csv"))
+api = api.drop(api.iloc[:, 1] == "DEPRECATED")
 done = ~(api.iloc[:, 1] == "TODO")
 print("{:.1f}% done ({}/{})".format(done.mean()*100, done.sum(), len(done)))
 
