2eeb679ddcdca1aabb074f143010c29497400bbc
RomeoDespres <romeo.despres@ens-rennes.fr>
2019-02-13 11:40:44 +0100

    Add TimeSelection.start setter

--- reapy/core/project/time_selection.py
+++ reapy/core/project/time_selection.py
@@ -7,9 +7,33 @@ class TimeSelection:
         
     @property
     def start(self):
+        """
+        Return time selection start in seconds.
+        
+        Returns
+        -------
+        start : float
+            Time selection start in seconds.
+        """
         _, _, _, start, _, _ = RPR.GetSet_LoopTimeRange2(
-            self.project_id, False, None, None, None, None
+            self.project_id, False, False, 0, 0, False
         )
         return start
+        
+    @start.setter
+    def start(self, start):
+        """
+        Set time selection start.
+        
+        Parameters
+        ----------
+        start : float
+            New time selection start.
+        """
+        infos = RPR.GetSet_LoopTimeRange2(
+            self.project_id, False, False, 0, 0, False
+        )
+        infos[1], infos[3] = True, start
+        RPR.GetSet_LoopTimeRange2(*infos) 
         
 
