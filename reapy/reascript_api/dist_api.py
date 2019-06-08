ee13fdf30dabeba707d2333efb1beb81fdf761dc
Konstantin Khlyzov <kostik.vento@gmail.com>
2019-06-09 03:30:12 +0500

    improve: remove unused imports

--- reapy/reascript_api/dist_api.py
+++ reapy/reascript_api/dist_api.py
@@ -1,8 +1,6 @@
 import reapy
 if not reapy.is_inside_reaper():
     try:
-        from reapy.tools import Program
-        import sys
         from reapy_generated_api import __all__
         from reapy_generated_api import *
     except ImportError:  # Happens when ``reapy`` dist API is disabled
