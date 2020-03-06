6d0ff48652b914a65b9109e37c701c4d95842b16
Timofey <pianoist@ya.ru>
2020-03-07 02:15:48 +0700

    connect fix + type hints
    connect now raises error on connection to the distant host, type hints for networking package are updated

--- reapy/tools/__init__.pyi
+++ reapy/tools/__init__.pyi
@@ -1,4 +1,13 @@
 """Define tools such as Program and custom json module."""
 
-# import reapy
-from ._inside_reaper import inside_reaper as inside_reaper
+import reapy
+from ._inside_reaper import inside_reaper, dist_api_is_enabled
+from .network.machines import connect, connect_to_default_machine, reconnect
+
+__all__ = [
+    'inside_reaper',
+    'dist_api_is_enabled',
+    'connect',
+    'connect_to_default_machine',
+    'reconnect',
+]
