ef14d86f10d6183f621688c92aa068f6951cd355
Timofey <pianoist@ya.ru>
2020-03-25 03:15:23 +0700

    detached midi_source, midi_Dest from sws

--- reapy/tools/__init__.py
+++ reapy/tools/__init__.py
@@ -3,4 +3,4 @@
 import reapy
 from ._inside_reaper import inside_reaper, dist_api_is_enabled
 from .network.machines import connect, connect_to_default_machine, reconnect
-from .extension_dependency import depends_on_sws
+from .extension_dependency import depends_on_sws, depends_on_extension
