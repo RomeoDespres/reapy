2c6d8a728fad9788d819f0a51bd876fd4ccd05b9
Timofey <pianoist@ya.ru>
2020-03-25 00:11:47 +0700

    track instantiation from the pointer

--- reapy/core/track/track.py
+++ reapy/core/track/track.py
@@ -45,6 +45,8 @@ class Track(ReapyObject):
 
     def __init__(self, id, project=None):
         self._project = None
+        if isinstance(id, (int, float)) and id > 10000:  # id is pointer
+            id = self._get_id_from_pointer(id)
         if isinstance(id, int):  # id is a track index
             id = RPR.GetTrack(project.id, id)
             if id.endswith("0x0000000000000000"):
@@ -63,6 +65,11 @@ class Track(ReapyObject):
     @property
     def _args(self):
         return self.id,
+
+    @classmethod
+    def _get_id_from_pointer(cls, id_):
+        int_ = int(id_)
+        return '(MediaTrack*)0x{0:0{1}X}'.format(int_, 16)
 
     @reapy.inside_reaper()
     def _get_project(self):
