92d3aeb690a7c62a39028cbf925774ea85f80e7b
Timofey <pianoist@ya.ru>
2020-03-24 14:38:55 +0700

    Major changes draft
    discussion is needed

--- reapy/core/track/__init__.pyi
+++ reapy/core/track/__init__.pyi
@@ -1,9 +1,12 @@
 from .automation_item import AutomationItem
 from .send import Send
-from .track import Track, TrackList
+from .track import Track, TrackList, SoloState, RecMode, RecMonitor
 __all__ = [
     'AutomationItem',
     'Send',
     'Track',
     'TrackList',
+    'SoloState',
+    'RecMode',
+    'RecMonitor',
 ]
