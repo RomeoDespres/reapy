88f0c4a7a1311f4010fc6383f517aeb673f4bbc7
Roméo Després <despres.romeo@gmail.com>
2020-05-15 22:35:04 +0200

    List Apple REAPER resource directories

--- reapy/config/reaper_resource_directory.py
+++ reapy/config/reaper_resource_directory.py
@@ -4,8 +4,14 @@ import sys
 from .shared_library import is_windows, is_apple
 
 
+def get_apple_candidates():
+    yield os.path.expanduser(
+        os.path.join('~', 'Library', 'Application Support', 'REAPER')
+    )
+
+
 def get_windows_candidates():
     # Normal installation
-    yield os.path.join(os.path.expandvars('$APPDATA'), 'REAPER')
+    yield os.path.expandvars(os.path.join('$APPDATA', 'REAPER'))
     # Default path for portable installations
     yield os.path.abspath(r'\REAPER')
