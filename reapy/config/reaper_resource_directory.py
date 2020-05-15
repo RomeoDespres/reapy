00fec6c2af0bfe6a794bcf74678916f4c8e537b6
RomeoDespres <despres.romeo@gmail.com>
2020-05-15 23:10:58 +0200

    List Linux REAPER resource directories

--- reapy/config/reaper_resource_directory.py
+++ reapy/config/reaper_resource_directory.py
@@ -5,13 +5,22 @@ from .shared_library import is_windows, is_apple
 
 
 def get_apple_candidates():
-    yield os.path.expanduser(
-        os.path.join('~', 'Library', 'Application Support', 'REAPER')
-    )
+    # Normal installation
+    yield os.path.expanduser('~/Library/Application Support/REAPER')
+
+
+def get_linux_candidates():
+    # Default path for portable installation
+    yield os.path.expanduser('~/.config/REAPER')
+    # Default path for all-users full installation
+    yield '/opt/REAPER'
+    # Default path for current user only full installation
+    yield os.path.expanduser('~/opt/REAPER')
 
 
 def get_windows_candidates():
     # Normal installation
-    yield os.path.expandvars(os.path.join('$APPDATA', 'REAPER'))
-    # Default path for portable installations
+    yield os.path.expandvars(r'$APPDATA\REAPER'))
+    # Default path for portable installation
     yield os.path.abspath(r'\REAPER')
+
