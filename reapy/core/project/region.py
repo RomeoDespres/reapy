b19bbaf22d79485812105713ed7811a3c79a33a8
Timofey <pianoist@ya.ru>
2021-01-14 09:59:33 +0700

    fix enum_index parameter in init of region

--- reapy/core/project/region.py
+++ reapy/core/project/region.py
@@ -21,7 +21,12 @@ class Region(ReapyObject):
         self.project = reapy.Project(parent_project_id)
         self.project_id = parent_project_id
         if index is None:
-            index = len(self.project.regions)
+            if enum_index is None:
+                index = len(self.project.regions)
+            else:
+                index = RPR.EnumProjectMarkers2(
+                    self.project_id, enum_index, 0, 0, 0, 0, 0
+                )[7]
         self.index = index
         if enum_index is None:
             enum_index = self._get_enum_index()
